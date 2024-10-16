ed4a3fc7c14ea02fd86e1e679cc35790ca33fef1
gonbeqt <143175525+gonbeqt@users.noreply.github.com>
2024-10-16 19:24:41 +0800

    FIX DESIGN

--- app/src/main/java/com/example/admin/SplashActivity.kt
+++ app/src/main/java/com/example/admin/SplashActivity.kt
@@ -1,32 +1,38 @@
 package com.example.admin
 
 import android.content.Intent
+import android.content.pm.ActivityInfo
+import android.net.Uri
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.view.animation.AnimationUtils
 import android.widget.ImageView
+import android.widget.VideoView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
 
 class SplashActivity : AppCompatActivity() {
-    private lateinit var imageView: ImageView
+    private lateinit var videoView: VideoView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_splash)
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
+        videoView = findViewById(R.id.videoView)
 
-        imageView = findViewById(R.id.noteszoom)
-        val zoomInAnimation = AnimationUtils.loadAnimation(this, R.anim.zoom_in)
-        imageView.startAnimation(zoomInAnimation)
+        val uri: Uri = Uri.parse("android.resource://$packageName/${R.raw.intro}")
+        videoView.setVideoURI(uri)
+        videoView.requestFocus()
+        videoView.start()
 
         Handler(Looper.getMainLooper()).postDelayed({
             val intent = Intent(this, AdminActivity::class.java)
             startActivity(intent)
             finish()
-        }, 2500)
+        }, 4500)
 
     }
 }
